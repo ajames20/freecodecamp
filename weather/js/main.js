d4ed3d3799848afb184ccc971546404e81456408
Andrew James <ajames20@gmail.com>
2017-02-08 09:05:51 +0200

    new projects completed

---
+++ weather/js/main.js
@@ -0,0 +1,48 @@
+/* RADOM QUOTE GENERATION */
+"use strict";
+
+(function() {
+    let loadError = document.getElementById('error'),
+        city = document.getElementById('city'),
+        description = document.getElementById('description'),
+        temp = document.getElementById('temp'),
+        icon = document.getElementById('icon');
+
+    const url = "http://api.wunderground.com/api/b6ad6560d07587c6/geolookup/conditions/q/autoip.json";
+
+
+    fetch(url, {
+            mode: 'cors'
+        })
+        .then((resp) => resp.json())
+        .then(function(data) {
+            // console.log(data);
+            city.innerHTML = '<h2>' + `${data.location.city}` + '</h2>';
+            temp.innerHTML = '<h3>' + `${data.current_observation.dewpoint_c}` + "<span>C</span>" + '</h3>';
+            description.innerHTML = "<h4>condition: " + '<span>' + `${data.current_observation.icon}` + '</span>' + "</h4>";
+            icon.innerHTML = "<img src=" + `${data.current_observation.icon_url}` + ">";
+
+
+
+            temp.addEventListener('click', function() {
+
+                let state1 = '<h3>' + `${data.current_observation.dewpoint_c}` + "<span>C</span>" + '</h3>';
+                let state2 = '<h3>' + `${data.current_observation.dewpoint_f}` + "<span>F</span>" + '</h3>';
+
+
+                if (temp.innerHTML == state1) {
+                    temp.innerHTML = state2;
+                } else if (temp.innerHTML == state2) {
+                    temp.innerHTML = state1;
+                } else {
+                    temp.innerHTML = state1;
+                }
+
+            });
+
+        })
+        .catch(function(error) {
+            // If there is any error you will catch them here
+            console.log(error);
+        });
+})();
