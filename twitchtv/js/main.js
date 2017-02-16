ddf4c4942aec9f45438b95c71ab2209f29f2efd1
Andrew James <ajames20@gmail.com>
2017-02-16 11:34:29 +0200

    twitch tv functionality updated

--- twitchtv/js/main.js
+++ twitchtv/js/main.js
@@ -4,6 +4,8 @@
 (function() {
     var loadError = document.getElementById('error'),
         main = document.getElementById('main-content'),
+        online = document.getElementById('online-user'),
+        offline = document.getElementById('offline-user'),
         allUsersButton = document.getElementById('all-users'),
         usersOfflineButton = document.getElementById('offline'),
         usersOnlineButton = document.getElementById('online'),
@@ -12,6 +14,7 @@
     for (var i = 0; i < streamers.length; i++) {
         getUserStreams(i);
     }
+
 
     function getUserStreams() {
         var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + streamers[i];
@@ -22,7 +25,6 @@
         }).then(function(resp) {
             return resp.json();
         }).then(function(data) {
-
             if (data.stream !== null && data.stream !== undefined) {
 
                 var profilePicture = data.stream.channel.logo,
@@ -30,15 +32,50 @@
                     userName = data.stream.channel.name,
                     game = data.stream.game;
 
-                main.innerHTML += '<div class="content online">' + '<h2>' + userName + '</h2>' + '<p>' + game + '</p>' + '<a href="' + channelLink + '" target="_blank">' + channelLink + '</a>' + '<img src="' + profilePicture + '">';
+
+                online.innerHTML += '<div class="content user-online-status">' + '<h2>' + userName + '</h2>' + '<p>' + game + '</p>' + '<a href="' + channelLink + '" target="_blank">' + channelLink + '</a>' + '<img src="' + profilePicture + '">';
+
             } else if (data.stream === null) {
                 var offChannel = data._links.channel,
                     userNameFromURL = data._links.channel.slice(38),
                     offChannelLink = data._links.self,
                     profilePicture = 'https://lh3.googleusercontent.com/Ti7DQ5NH92V-_uuOiTbMOMwAjVXzl2rubjygxQp3KGutzuRHnjYvpCtfWJybeDYh4g=w300',
-                    game = '<p>Offline</p>';
+                    game = '<p>Offline</p>',
+                    url = 'https://wind-bow.gomix.me/twitch-api/channels/' + userNameFromURL;
 
-                main.innerHTML += '<div class="content offline">' + '<h2>' + userNameFromURL + '</h2>' + '<p>' + game + '</p>' + '<a href="' + offChannelLink + '" target="_blank">' + offChannelLink + '</a>' + '<img src="' + profilePicture + '">';
+
+                fetch(url, {
+                    method: 'get'
+
+                }).then(function(resp) {
+                    return resp.json();
+                }).then(function(data) {
+
+                    var profilePicture = data.logo,
+                        channelLink = data.url,
+                        userName = data.name,
+                        game = data.game;
+
+                    if (game === null || game === undefined) {
+                        game = 'No channel Information';
+                    }
+                    if (userName === null) {
+                        name = "not found";
+                    }
+                    if (profilePicture === null || profilePicture === undefined) {
+                        profilePicture = 'https://lh3.googleusercontent.com/Ti7DQ5NH92V-_uuOiTbMOMwAjVXzl2rubjygxQp3KGutzuRHnjYvpCtfWJybeDYh4g=w300';
+                    }
+                    if (data.status === 404 || data.error === "Not Found") {
+                        offline.innerHTML = '<div class="content no-user">' + '<h2>' + data.error + '</h2>' + '<p>' + data.message + '</p>' + '<a href="https://twitch.tv" target="_blank"> https://twitch.tv</a>' + '<img src="' + profilePicture + '">';
+                    } else {
+                        offline.innerHTML += '<div class="content user-offline-status">' + '<h2>' + userName + '</h2>' + '<p>' + game + '</p>' + '<a href="' + channelLink + '" target="_blank">' + channelLink + '</a>' + '<img src="' + profilePicture + '">';
+                    }
+
+                }).catch(function(error) {
+                    // If there is any error you will catch them here
+                    loadError.innerHTML = '<h2 class="error">There was a problem fiding what you were looking for!</h2>';
+                });
+
             }
         }).catch(function(error) {
             // If there is any error you will catch them here
@@ -46,34 +83,58 @@
         });
     }
 
+
+
     allUsersButton.addEventListener('click', function() {
-        var elem = document.querySelectorAll('.content');
+        var elem = document.querySelectorAll('.content'),
+            elem2 = document.querySelectorAll('.user-online-status'),
+            elem3 = document.querySelectorAll('.user-offline-status');
+
         for (var i = 0; i < elem.length; i++) {
             elem[i].classList.remove('hidden');
         }
+        for (var i = 0; i < elem2.length; i++) {
+            elem2[i].classList.remove('online');
+        }
+        for (var i = 0; i < elem3.length; i++) {
+            elem3[i].classList.remove('offline');
+        }
+
     });
 
     usersOnlineButton.addEventListener('click', function() {
-        var elem = document.querySelectorAll('.offline'),
-            elemAdd = document.querySelectorAll('.online');
+        var elem = document.querySelectorAll('.user-online-status'),
+            elem2 = document.querySelectorAll('.no-user'),
+            elem3 = document.querySelectorAll('.user-offline-status');
 
+        console.log(elem);
         for (var i = 0; i < elem.length; i++) {
-            elem[i].classList.add('hidden');
+            elem[i].classList.add('online');
+            elem[i].classList.remove('hidden');
         }
-        for (var i = 0; i < elemAdd.length; i++) {
-            elemAdd[i].classList.remove('hidden');
+        for (var i = 0; i < elem2.length; i++) {
+            elem2[i].classList.add('hidden');
+        }
+        for (var i = 0; i < elem3.length; i++) {
+            elem3[i].classList.add('hidden');
         }
     });
 
     usersOfflineButton.addEventListener('click', function() {
-        var elem = document.querySelectorAll('.online'),
-            elemAdd = document.querySelectorAll('.offline');
+        var elem = document.querySelectorAll('.user-online-status'),
+            elem2 = document.querySelectorAll('.no-user'),
+            elem3 = document.querySelectorAll('.user-offline-status');
 
         for (var i = 0; i < elem.length; i++) {
             elem[i].classList.add('hidden');
         }
-        for (var i = 0; i < elemAdd.length; i++) {
-            elemAdd[i].classList.remove('hidden');
+        for (var i = 0; i < elem2.length; i++) {
+            elem2[i].classList.add('hidden');
+        }
+        for (var i = 0; i < elem3.length; i++) {
+            elem3[i].classList.add('offline');
+            elem3[i].classList.remove('hidden');
         }
     });
-})();
+
+})();;
